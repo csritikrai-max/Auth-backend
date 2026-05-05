f68db037670621ace1bcfd66ce954e2375130fbe
Ritik Rai <cs.ritikrai@gmail.com>
2026-05-05 23:19:49 +0530

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const index = express();
 
 index.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://auth-frontend-gamma-taupe.vercel.app/",
     credentials: true,
 }));
 
